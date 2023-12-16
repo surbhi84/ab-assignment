b08101b9c95a00434079c54f4eb131f58be84824
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2023-12-16 21:38:49 +0530

    adding font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Open Sans", "sans-serif"],
+      },
+    },
     colors: {
+      abBlack: "rgba(0,0,0,1)",
       abWhite: "rgba(255, 255, 255, 1)",
+      abLightGray: "rgba(217, 217, 217, 1)",
+      abBtn: "rgba(0, 68, 193, 1)",
       sidebarBg: "#2D2D2D",
     },
   },
